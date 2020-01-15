21fc6bcbadb6505de169e21515e40aeca9bfd318
pygmelion <741563052@qq.com>
2020-01-15 17:31:59 +0800

    rebuild highlight

--- 前后端代码/HighLight.java
+++ 前后端代码/HighLight.java
@@ -1,68 +1,109 @@
-import java.awt.EventQueue;
+package com.codeborne.selenide.logevents;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Warning {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-	JFrame jfframe;
-	public String t;
+import static com.codeborne.selenide.logevents.LogEvent.EventStatus.FAIL;
 
-	/**
-	 * Launch the application.
-	 */
+public class SelenideLogger {
+  private static final Logger LOG = LoggerFactory.getLogger(SelenideLogger.class);
 
-	/**
-	 * Create the application.
-	 */
-	public Warning(String _text) {
-		this.t = _text;
-		initialize();
-	}
-	
-	public static void ShowWarning(String s) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Warning window = new Warning(s);
-					window.jfframe.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  protected static ThreadLocal<Map<String, LogEventListener>> listeners = new ThreadLocal<>();
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		jfframe = new JFrame();
-		jfframe.setBounds(100, 100, 629, 279);
-		jfframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jfframe.getContentPane().setLayout(null);
-		
-		JButton button = new JButton("确定");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				jfframe.dispose();
-			}
-		});
-		button.setBounds(427, 173, 97, 23);
-		
-		JLabel lblNewLabel = new JLabel(this.t);
-		lblNewLabel.setFont(new Font("黑体", Font.PLAIN, 17));
-		lblNewLabel.setBounds(54, 38, 495, 25);
-		jfframe.getContentPane().add(lblNewLabel);
-		
-		JLabel label = new JLabel("通知：");
-		label.setFont(new Font("黑体", Font.PLAIN, 17));
-		label.setBounds(10, 10, 79, 30);
-		jfframe.getContentPane().add(label);
-		jfframe.getContentPane().add(button);
-	}
+  public static void addListener(String name, LogEventListener listener) {
+    Map<String, LogEventListener> threadListeners = listeners.get();
+    if (threadListeners == null) {
+      threadListeners = new HashMap<>();
+    }
+
+    threadListeners.put(name, listener);
+    listeners.set(threadListeners);
+  }
+
+  public static SelenideLog beginStep(String source, String methodName, Object... args) {
+    return beginStep(source, readableMethodName(methodName) + "(" + readableArguments(args) + ")");
+  }
+
+  static String readableMethodName(String methodName) {
+    return methodName.replaceAll("([A-Z])", " $1").toLowerCase();
+  }
+
+  static String readableArguments(Object... args) {
+    return args == null ? "" :
+        (args[0] instanceof Object[]) ? arrayToString((Object[]) args[0]) :
+            arrayToString(args);
+  }
+
+  private static String arrayToString(Object[] args) {
+    return args.length == 1 ? String.valueOf(args[0]) : Arrays.toString(args);
+  }
+
+  public static SelenideLog beginStep(String source, String subject) {
+    Collection<LogEventListener> listeners = getEventLoggerListeners();
+
+    SelenideLog log = new SelenideLog(source, subject);
+    for (LogEventListener listener : listeners) {
+      try {
+        listener.beforeEvent(log);
+      }
+      catch (RuntimeException e) {
+        LOG.error("Failed to call listener {}", listener, e);
+      }
+    }
+    return log;
+  }
+
+  public static void commitStep(SelenideLog log, Throwable error) {
+    log.setError(error);
+    commitStep(log, FAIL);
+  }
+
+  public static void commitStep(SelenideLog log, LogEvent.EventStatus status) {
+    log.setStatus(status);
+
+    Collection<LogEventListener> listeners = getEventLoggerListeners();
+    for (LogEventListener listener : listeners) {
+      try {
+        listener.afterEvent(log);
+      }
+      catch (RuntimeException e) {
+        LOG.error("Failed to call listener {}", listener, e);
+      }
+    }
+  }
+
+  private static Collection<LogEventListener> getEventLoggerListeners() {
+    if (listeners.get() == null) {
+      listeners.set(new HashMap<>());
+    }
+    return listeners.get().values();
+  }
+
+
+  @SuppressWarnings("unchecked")
+  public static <T extends LogEventListener> T removeListener(String name) {
+    Map<String, LogEventListener> listeners = SelenideLogger.listeners.get();
+    return listeners == null ? null : (T) listeners.remove(name);
+  }
+
+  public static void removeAllListeners() {
+    SelenideLogger.listeners.remove();
+  }
+
+  /**
+   * If listener with given name is bound (added) to the current thread.
+   *
+   * @param name unique name of listener added by method {@link #addListener(String, LogEventListener)}
+   * @return true if method {@link #addListener(String, LogEventListener)} with
+   *              corresponding name has been called in current thread.
+   */
+  public static boolean hasListener(String name) {
+    Map<String, LogEventListener> listeners = SelenideLogger.listeners.get();
+    return listeners != null && listeners.containsKey(name);
+  }
 }
